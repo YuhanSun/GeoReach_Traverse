f15997f1fb22eaa1a95b582a46085a0439951c1c
Yuhan Sun <wdmzssyh@gmail.com>
2018-04-17 19:46:31 -0700

    ini

---
+++ GeoReach_Traverse/src/main/java/experiment/Prepare.java
@@ -0,0 +1,47 @@
+package experiment;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import commons.Config;
+import commons.Config.Datasets;
+import commons.Util;
+
+public class Prepare {
+
+	static String[] datasets = new String[] {
+			Datasets.foursquare.toString(), 
+			Datasets.Gowalla_10.toString(), 
+			Datasets.foursquare.toString()
+			};
+	
+	static Config config = new Config();
+	static String projectDir = config.getProjectDir();
+	static String dataDir = config.getDataDir();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		generateStartID();
+	}
+
+	public static void generateStartID()
+	{
+		for (String dataset : datasets)
+		{
+			String outputPath = String.format("%s\\query\\%s\\startID.txt", 
+					projectDir, dataset);
+			String graphPath = String.format("%s\\%s\\graph.txt", 
+					dataDir, dataset);
+			
+			Util.Print("Get graph node count from " + graphPath);
+			int nodeCount = Util.GetNodeCountGeneral(graphPath);
+			HashSet<Long> idSet = Util.GenerateRandomInteger(nodeCount, 500);
+			ArrayList<String> idStrings = new ArrayList<>(idSet.size());
+			for (Long id : idSet)
+				idStrings.add(String.valueOf(id));
+			Util.Print("Output to " + outputPath);
+			Util.WriteArray(outputPath, idStrings);
+		}
+		
+	}
+}
