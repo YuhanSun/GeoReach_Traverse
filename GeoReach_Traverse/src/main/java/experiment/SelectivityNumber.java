a8446a16e0d03cd7f0a5dea0477d9149bee2ee86
Yuhan Sun <wdmzssyh@gmail.com>
2018-04-21 20:54:15 -0700

    update

--- GeoReach_Traverse/src/main/java/experiment/SelectivityNumber.java
+++ GeoReach_Traverse/src/main/java/experiment/SelectivityNumber.java
@@ -1,11 +1,20 @@
 package experiment;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import commons.Config;
 import commons.Entity;
+import commons.MyRectangle;
+import commons.OwnMethods;
+import commons.Query_Graph;
 import commons.Util;
+import commons.Utility;
 import commons.Config.system;
+import graph.RisoTreeQueryPN;
 
 public class SelectivityNumber {
 
@@ -16,17 +25,16 @@ public class SelectivityNumber {
 	public String password;
 	public int MAX_HOPNUM;
 
-	public String dataDir, projectDir;
+	public String dataDir, projectDir, dbDir;
 	public String db_path;
 	public String graph_pos_map_path;
 	public String entityPath;
 
-	public String querygraphDir;
-	public String spaPredicateDir;
+//	public String querygraphDir;
+	public String queryDir;
 	public String resultDir;
 
 	public boolean TEST_FORMAT;
-	public int experimentCount = 10;
 
 	//non-spatial ratio 20
 	//	static double startSelectivity = 0.000001;
@@ -59,6 +67,10 @@ public class SelectivityNumber {
 		initializeParameters();
 	}
 
+	public static int pieces_x = 128, pieces_y = 128;
+	public static double MG = 1.0, MR = 1.0;
+	public static int MC = 0;
+	
 	public void initializeParameters()
 	{	
 		TEST_FORMAT = false;
@@ -67,23 +79,29 @@ public class SelectivityNumber {
 		systemName = config.getSystemName();
 		password = config.getPassword();
 		MAX_HOPNUM = config.getMaxHopNum();
+		
+		dbDir = config.getDBDir();
+		projectDir = config.getProjectDir();
+		dataDir = config.getDataDir();
+		
 		switch (systemName) {
 		case Ubuntu:
-			db_path = String.format("/home/yuhansun/Documents/GeoGraphMatchData/%s_%s/data/databases/graph.db", version, dataset);
-			graph_pos_map_path = "/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/" + dataset + "/node_map_RTree.txt";
-			entityPath = String.format("/mnt/hgfs/Ubuntu_shared/GeoMinHop/data/%s/entity.txt", dataset);
-			querygraphDir = String.format("/mnt/hgfs/Google_Drive/Projects/risotree/query/query_graph/%s", dataset);
-			spaPredicateDir = String.format("/mnt/hgfs/Google_Drive/Projects/risotree/query/spa_predicate/%s", dataset);
-			resultDir = String.format("/mnt/hgfs/Google_Drive/Experiment_Result/Riso-Tree/%s", dataset);
+			String dbFolder = String.format("%s_%d_%d_%d_%d_%d_%d", version, pieces_x, pieces_y, (int) (MG * 100), (int) (MR * 100), MC, 3);
+			db_path = String.format("%s/%s/%s/data/databases/graph.db", dbDir, dataset, dbFolder);
+			graph_pos_map_path = dataDir + "/" + dataset + "/node_map_RTree.txt";
+			entityPath = String.format("%s/%s/entity.txt", dataDir, dataset);
+//			querygraphDir = String.format("/mnt/hgfs/Google_Drive/Projects/risotree/query/query_graph/%s", dataset);
+			queryDir = String.format("%s/query/%s", projectDir, dataset);
+			resultDir = String.format("%s/selectivity_number/%s", projectDir, dataset);
 			//			resultDir = String.format("/mnt/hgfs/Google_Drive/Experiment_Result/Riso-Tree/%s/switch_point", dataset);
 			break;
 		case Windows:
-			db_path = String.format("D:\\Ubuntu_shared\\GeoMinHop\\data\\%s\\%s_%s\\data\\databases\\graph.db", dataset, version, dataset);
-			graph_pos_map_path = "D:\\Ubuntu_shared\\GeoMinHop\\data\\" + dataset + "\\node_map_RTree.txt";
-			entityPath = String.format("D:\\Ubuntu_shared\\GeoMinHop\\data\\%s\\entity.txt", dataset);
+//			db_path = String.format("D:\\Ubuntu_shared\\GeoMinHop\\data\\%s\\%s_%s\\data\\databases\\graph.db", dataset, version, dataset);
+//			graph_pos_map_path = "D:\\Ubuntu_shared\\GeoMinHop\\data\\" + dataset + "\\node_map_RTree.txt";
+//			entityPath = String.format("D:\\Ubuntu_shared\\GeoMinHop\\data\\%s\\entity.txt", dataset);
 			//			querygraphDir = String.format("D:\\Ubuntu_shared\\GeoMinHop\\query\\query_graph\\%s", dataset);
 			//			spaPredicateDir = String.format("D:\\Ubuntu_shared\\GeoMinHop\\query\\spa_predicate\\%s", dataset);
-			resultDir = String.format("D:\\Google_Drive\\Experiment_Result\\Riso-Tree\\%s", dataset);
+//			resultDir = String.format("D:\\Google_Drive\\Experiment_Result\\Riso-Tree\\%s", dataset);
 			break;
 		}
 		ArrayList<Entity> entities = Util.ReadEntity(entityPath);
@@ -91,8 +109,182 @@ public class SelectivityNumber {
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Config config = new Config();
+		
+//		ArrayList<String> dataset_a = new ArrayList<String>(Arrays.asList(
+//				Config.Datasets.Gowalla_100.name(), 
+//				Config.Datasets.foursquare_100.name(),
+//				Config.Datasets.Patents_100_random_80.name(), 
+//				Config.Datasets.go_uniprot_100_random_80.name()));
+		
+		config.setDatasetName(Config.Datasets.foursquare_100.name());
+		SelectivityNumber selectivityNumber = new SelectivityNumber(config);
+		
+		//Read start ids
+		String startIDPath = String.format("%s/startID.txt", selectivityNumber.queryDir);
+		ArrayList<Integer> startIDs = Util.readIntegerArray(startIDPath);
+		
+		int repeatTime = 10;
+		ArrayList<ArrayList<Long>> startIDsList = new ArrayList<>();
+		for ( int i = 0; i < repeatTime; i++)
+			startIDsList.add(new ArrayList<>());
+		
+		int i = 0;
+		for ( int id : startIDs)
+		{
+			int index = i % repeatTime;
+			startIDsList.get(index).add((long) id);
+			id++;
+		}
+		
+		selectivityNumber.simpleTraversal(startIDsList);
+	}
+	
+	public void simpleTraversal(ArrayList<ArrayList<Long>> startIDs)
+	{
+		try {
+			long start;
+			long time;
 
+			String result_detail_path = null, result_avg_path = null;
+			switch (systemName) {
+			case Ubuntu:
+				result_detail_path = String.format("%s/simpleTraversal.txt", resultDir);
+				result_avg_path = String.format("%s/risotree_PN%d_%d_%d_avg.txt", resultDir, MAX_HOPNUM, nodeCount, query_id);
+				//				result_detail_path = String.format("%s/risotree_%d_%d_test.txt", resultDir, nodeCount, query_id);
+				//				result_avg_path = String.format("%s/risotree_%d_%d_avg_test.txt", resultDir, nodeCount, query_id);
+				break;
+			case Windows:
+//				result_detail_path = String.format("%s\\risotree_PN_%d_%d.txt", resultDir, nodeCount, query_id);
+//				result_avg_path = String.format("%s\\risotree_PN_%d_%d_avg.txt.txt", resultDir, nodeCount, query_id);
+				break;
+			}
+
+			String write_line = String.format("%s\t%d\n", dataset, limit);
+			if(!TEST_FORMAT)
+			{
+				OwnMethods.WriteFile(result_detail_path, true, write_line);
+				OwnMethods.WriteFile(result_avg_path, true, write_line);
+			}
+
+			String head_line = "count\trange_time\tget_iterator_time\titerate_time\ttotal_time\taccess_pages\n";
+			if(!TEST_FORMAT)
+				OwnMethods.WriteFile(result_avg_path, true, "selectivity\t" + head_line);
+
+			double selectivity = startSelectivity;
+			int times = 10;
+			while ( selectivity <= endSelectivity)
+			{
+				int name_suffix = (int) (selectivity * spaCount);
+
+				String queryrect_path = null;
+				switch (systemName) {
+				case Ubuntu:
+					queryrect_path = String.format("%s/queryrect_%d.txt", spaPredicateDir, name_suffix);
+					break;
+				case Windows:
+					queryrect_path = String.format("%s\\queryrect_%d.txt", spaPredicateDir, name_suffix);
+					break;
+				}
+
+				write_line = selectivity + "\n" + head_line;
+				if(!TEST_FORMAT)
+					OwnMethods.WriteFile(result_detail_path, true, write_line);
+
+				ArrayList<MyRectangle> queryrect = OwnMethods.ReadQueryRectangle(queryrect_path);
+				HashMap<String, String> graph_pos_map = OwnMethods.ReadMap(graph_pos_map_path);
+				long[] graph_pos_map_list= new long[graph_pos_map.size()];
+				for ( String key_str : graph_pos_map.keySet())
+				{
+					int key = Integer.parseInt(key_str);
+					int pos_id = Integer.parseInt(graph_pos_map.get(key_str));
+					graph_pos_map_list[key] = pos_id;
+				}
+				RisoTreeQueryPN risoTreeQueryPN = new RisoTreeQueryPN(db_path, dataset, 
+						graph_pos_map_list, MAX_HOPNUM);
+
+				ArrayList<Long> range_query_time = new ArrayList<Long>();
+				ArrayList<Long> time_get_iterator = new ArrayList<Long>();
+				ArrayList<Long> time_iterate = new ArrayList<Long>();
+				ArrayList<Long> total_time = new ArrayList<Long>();
+				ArrayList<Long> count = new ArrayList<Long>();
+				ArrayList<Long> access = new ArrayList<Long>();
+
+				for ( int i = 0; i < experimentCount; i++)
+				{
+					MyRectangle rectangle = queryrect.get(i);
+					if ( rectangle.area() == 0.0)
+					{
+						double delta = Math.pow(0.1, 10);
+						rectangle = new MyRectangle(rectangle.min_x - delta, rectangle.min_y - delta,
+								rectangle.max_x + delta, rectangle.max_y + delta);
+					}
+
+					query_Graph.spa_predicate = new MyRectangle[query_Graph.graph.size()];
+
+					//only handle query with one spatial predicate
+					int j = 0;
+					for (  ; j < query_Graph.graph.size(); j++)
+						if(query_Graph.Has_Spa_Predicate[j])
+							break;
+					query_Graph.spa_predicate[j] = rectangle;
+
+					if(!TEST_FORMAT)
+					{
+						OwnMethods.Print(String.format("%d : %s", i, rectangle.toString()));
+
+						start = System.currentTimeMillis();
+						risoTreeQueryPN.Query(query_Graph, -1);
+						time = System.currentTimeMillis() - start;
+
+						time_get_iterator.add(risoTreeQueryPN.get_iterator_time);
+						time_iterate.add(risoTreeQueryPN.iterate_time);
+						total_time.add(time);
+						count.add(risoTreeQueryPN.result_count);
+						access.add(risoTreeQueryPN.page_hit_count);OwnMethods.Print("Page access:" + risoTreeQueryPN.page_hit_count);
+						range_query_time.add(risoTreeQueryPN.range_query_time);
+
+						write_line = String.format("%d\t%d\t", count.get(i), range_query_time.get(i));
+						write_line += String.format("%d\t", time_get_iterator.get(i));
+						write_line += String.format("%d\t%d\t", time_iterate.get(i), total_time.get(i));
+						write_line += String.format("%d\n", access.get(i));
+						if(!TEST_FORMAT)
+							OwnMethods.WriteFile(result_detail_path, true, write_line);
+					}
+
+					risoTreeQueryPN.dbservice.shutdown();
+
+					OwnMethods.ClearCache(password);
+					Thread.currentThread();
+					Thread.sleep(5000);
+
+					risoTreeQueryPN.dbservice = new GraphDatabaseFactory().newEmbeddedDatabase(new File(db_path));
+
+				}
+				risoTreeQueryPN.dbservice.shutdown();
+
+				write_line = String.valueOf(selectivity) + "\t";
+				write_line += String.format("%d\t%d\t", Utility.Average(count), Utility.Average(range_query_time));
+				write_line += String.format("%d\t", Utility.Average(time_get_iterator));
+				write_line += String.format("%d\t%d\t", Utility.Average(time_iterate), Utility.Average(total_time));
+				write_line += String.format("%d\n", Utility.Average(access));
+				if(!TEST_FORMAT)
+					OwnMethods.WriteFile(result_avg_path, true, write_line);
+
+				//				long larger_time = Utility.Average(total_time);
+				//				if (larger_time * expe_count > 450 * 1000)
+				//					expe_count = (int) (expe_count * 0.5 / (larger_time * expe_count / 450.0 / 1000.0));
+				//				if(expe_count < 1)
+				//					expe_count = 1;
+
+				selectivity *= times;
+			}
+			OwnMethods.WriteFile(result_detail_path, true, "\n");
+			OwnMethods.WriteFile(result_avg_path, true, "\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 
 }
--- GeoReach_Traverse/src/test/java/query/SimpleGraphTraversalTest.java
+++ GeoReach_Traverse/src/test/java/query/SimpleGraphTraversalTest.java
@@ -2,9 +2,17 @@ package query;
 
 import static org.junit.Assert.*;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.neo4j.cypher.internal.frontend.v2_3.ast.functions.Length;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Result;
@@ -32,7 +40,9 @@ public class SimpleGraphTraversalTest {
 //			db_path = String.format("%s/%s_%s/data/databases/graph.db", dbDir, version, dataset);
 			break;
 		case Windows:
-			db_path = String.format("%s\\%s\\neo4j-community-3.1.1_128_128_0_100_0_3\\data\\databases\\graph.db", 
+//			db_path = String.format("%s\\%s\\neo4j-community-3.1.1_128_128_0_100_0_3\\data\\databases\\graph.db", 
+//					dbDir, dataset);
+			db_path = String.format("%s\\%s\\neo4j-community-3.1.1_128_128_100_100_0_3\\data\\databases\\graph.db", 
 					dbDir, dataset);
 		default:
 			break;
@@ -48,17 +58,23 @@ public class SimpleGraphTraversalTest {
 		try {
 			Util.Print(db_path);
 			SimpleGraphTraversal simpleGraphTraversal = new SimpleGraphTraversal(db_path);
-//			long startID = 768514;
-			long startID = 1299706;
 			GraphDatabaseService dbservice = simpleGraphTraversal.dbservice;
 			Transaction tx = dbservice.beginTx();
 			Node node = dbservice.getNodeById(startID);
 			Util.Print(node);
 			Util.Print(node.getLabels());
 			Util.Print(node.getAllProperties());
-			String query = String.format("match p = (s)-[:GRAPH_LINK]-(a)-[:GRAPH_LINK]-(c)-[:GRAPH_LINK]-(b)"
-					+ "where id(s) = %d and exists(b.%s) return p", 
-					startID, "lon");
+			String query = String.format("profile match p = (s)-[:GRAPH_LINK]-(a)-[:GRAPH_LINK]-(c)-[:GRAPH_LINK]-(b) "
+					+ "where id(s) = %d and %s < b.lon < %s and "
+					+ "%s < b.lat < %s "
+					+ "return p", 
+					startID, String.valueOf(queryRectangle.min_x), String.valueOf(queryRectangle.max_x),
+					String.valueOf(queryRectangle.min_y), String.valueOf(queryRectangle.max_y));
+//			String query = String.format("match p = (s)-[:GRAPH_LINK]-(a)-[:GRAPH_LINK]-(c)-[:GRAPH_LINK]-(b) "
+//					+ "where id(s) = %d and -120 < b.lon < -60 and "
+//					+ "30 < b.lat < 40 "
+//					+ "return p", 
+//					startID, "lon");
 			Result result = dbservice.execute(query);
 			int count = 0;
 			while (result.hasNext())
@@ -68,6 +84,10 @@ public class SimpleGraphTraversalTest {
 				count++;
 			}
 			Util.Print(count);
+			
+			long hits = Util.GetTotalDBHits(result.getExecutionPlanDescription());
+			Util.Print("DB hits: " + hits);
+			
 			tx.success();
 			tx.close();
 			dbservice.shutdown();
@@ -83,13 +103,19 @@ public class SimpleGraphTraversalTest {
 		try {
 			Util.Print(db_path);
 			SimpleGraphTraversal simpleGraphTraversal = new SimpleGraphTraversal(db_path);
-			long startID = 1299706;
 			GraphDatabaseService dbservice = simpleGraphTraversal.dbservice;
 			Transaction tx = dbservice.beginTx();
 			Node node = dbservice.getNodeById(startID);
-			simpleGraphTraversal.traversal(node, 2, new MyRectangle(-120, 30, -60, 40));
-			int count = simpleGraphTraversal.paths.size();
+//			simpleGraphTraversal.traversal(node, 2, new MyRectangle(-120, 30, -60, 40));
+			simpleGraphTraversal.traversal(node, Length, queryRectangle);
+			
+//			int count = simpleGraphTraversal.paths.size();
+			long count = simpleGraphTraversal.resultCount;
 			Util.Print(count);
+			
+			long visitedCount = simpleGraphTraversal.visitedCount;
+			Util.Print("Visited Count: " + visitedCount);
+			
 			tx.success();
 			tx.close();
 			dbservice.shutdown();
@@ -99,19 +125,37 @@ public class SimpleGraphTraversalTest {
 		}
 	}
 	
+	//The test query
+	public static long startID = 1299708;
+	ArrayList<Long> startIDs = new ArrayList<Long>(Arrays.asList(
+			startID
+			));
+	public static int Length = 3;
+	public static MyRectangle queryRectangle = new MyRectangle(-70, 30, -60, 40);
+	
 	@Test
 	public void spaTreversalTest() {
 		try {
 			Util.Print(db_path);
-			SpaTraversal spaTraversal = new SpaTraversal(db_path, 
+			SpaTraversal spaTraversal = new SpaTraversal(db_path, 2, 
 					new MyRectangle(-180, -90, 180, 90), 128, 128);
-			long startID = 1299706;
 			GraphDatabaseService dbservice = spaTraversal.dbservice;
 			Transaction tx = dbservice.beginTx();
 			Node node = dbservice.getNodeById(startID);
-			spaTraversal.traversal(node, 3, new MyRectangle(-180, -90, 180, 90));
-			int count = spaTraversal.paths.size();
-			Util.Print(count);
+//			spaTraversal.traversal(node, 2, new MyRectangle(-120, 30, -60, 40));
+			
+//			startIDs.add((long) 1299708);
+			LinkedList<Node> startNodes = new LinkedList<>();
+			for (long id : startIDs)
+				startNodes.add(dbservice.getNodeById(id));
+			
+			spaTraversal.traversal(startNodes, Length, queryRectangle);
+			
+			Util.Print(spaTraversal.resultCount);
+			Util.Print("Visited Count: " + spaTraversal.visitedCount);
+			Util.Print("GeoReachPrunedCount: " + spaTraversal.GeoReachPruneCount);
+			Util.Print("PrunedVerticesWorkCount: " + spaTraversal.PrunedVerticesWorkCount);
+			
 			tx.success();
 			tx.close();
 			dbservice.shutdown();
@@ -119,5 +163,73 @@ public class SimpleGraphTraversalTest {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
+	}
+	
+//	@Test
+//	public void compareTest() {
+//		try {
+//			Util.Print(db_path);
+//			SpaTraversal spaTraversal = new SpaTraversal(db_path, 
+//					new MyRectangle(-180, -90, 180, 90), 128, 128);
+//			long startID = 1299706;
+//			GraphDatabaseService dbservice = spaTraversal.dbservice;
+//			Transaction tx = dbservice.beginTx();
+//			Node node = dbservice.getNodeById(startID);
+//			spaTraversal.traversal(node, 2, new MyRectangle(-120, 30, -60, 40));
+//			int count = spaTraversal.paths.size();
+//			Util.Print(count);
+//			
+//			TreeSet<String> pathsStringSet1 = createPathStringList(spaTraversal.paths);
+//			Util.WriteFile("D:\\temp\\spa.txt", false, pathsStringSet1);
+//			
+//			tx.success();
+//			tx.close();
+//			dbservice.shutdown();
+//			
+//			//
+//			SimpleGraphTraversal simpleGraphTraversal = new SimpleGraphTraversal(db_path);
+//			dbservice = simpleGraphTraversal.dbservice;
+//			tx = dbservice.beginTx();
+//			node = dbservice.getNodeById(startID);
+//			simpleGraphTraversal.traversal(node, 2, new MyRectangle(-120, 30, -60, 40));
+//			count = simpleGraphTraversal.paths.size();
+//			Util.Print(count);
+//			
+//			TreeSet<String> pathsStringSet2 = createPathStringList(simpleGraphTraversal.paths);
+//			Util.WriteFile("D:\\temp\\simple.txt", false, pathsStringSet2);
+//			
+//			tx.success();
+//			tx.close();
+//			dbservice.shutdown();
+//			
+//			//compare
+//			Util.Print("Traverse not in Spa:");
+//			for (String path : pathsStringSet2)
+//			{
+//				if (pathsStringSet1.contains(path) == false)
+//					Util.Print(path);
+//			}
+//			
+//			Util.Print("Spa not in Traverse:");
+//			for (String path : pathsStringSet1)
+//			{
+//				if (pathsStringSet2.contains(path) == false)
+//					Util.Print(path);
+//			}
+//			
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//		}
+//	}
+	
+	static public TreeSet<String> createPathStringList(ArrayList<LinkedList<Long>> paths) 
+	{
+		TreeSet<String> pathsStringSet = new TreeSet<>();
+		
+		for (LinkedList<Long> path : paths)
+			pathsStringSet.add(path.toString());
+		
+		return pathsStringSet;
 	}
 }
