a3dc58fe70c063fb6b245f84327fe72d247f9bc0
YuhanSun <wdmzssyh@gmail.com>
2018-04-26 22:24:30 -0700

    ini

---
+++ GeoReach_Traverse/src/main/java/commons/VertexGeoReachList.java
@@ -0,0 +1,43 @@
+package commons;
+
+import java.util.ArrayList;
+
+/**
+ * GeoReach index for each vertex
+ * ReachGrids is list.
+ * @author ysun138
+ *
+ */
+public class VertexGeoReachList {
+	public ArrayList<Integer> types;
+	public ArrayList<ArrayList<Integer>> ReachGrids;
+	public ArrayList<MyRectangle> RMBRs;
+	public ArrayList<Boolean> GeoBs;
+	
+	public VertexGeoReachList(int MAX_HOP)
+	{
+		ReachGrids = new ArrayList<ArrayList<Integer>>(MAX_HOP);
+		RMBRs = new ArrayList<MyRectangle>(MAX_HOP);
+		GeoBs = new ArrayList<Boolean>(MAX_HOP);
+		for (int i = 0; i < MAX_HOP; i++)
+		{
+			ReachGrids.add(null);
+			RMBRs.add(null);
+			GeoBs.add(null);
+		}
+	}
+	
+	@Override
+	public String toString()
+	{
+		String string = "";
+		for ( int i = 0; i < ReachGrids.size(); i++)
+		{
+			string += ReachGrids.get(i);
+			string += ";" + RMBRs.get(i);
+			string += ";" + GeoBs.get(i);
+			string += "\n";
+		}
+		return string;
+	}
+}
