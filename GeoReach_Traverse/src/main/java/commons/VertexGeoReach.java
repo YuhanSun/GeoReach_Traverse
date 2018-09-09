d630e142e8d9727f593fbac33c61d5a11e927c5a
yuhansun <wdmzssyh@gmail.com>
2018-09-09 13:58:32 -0700

    add comment

--- GeoReach_Traverse/src/main/java/commons/VertexGeoReach.java
+++ GeoReach_Traverse/src/main/java/commons/VertexGeoReach.java
@@ -4,40 +4,37 @@ import java.util.ArrayList;
 import java.util.TreeSet;
 
 /**
- * GeoReach index for each vertex
+ * GeoReach index for each vertex. ** ReachGrid is TreeSet. **
+ *
  * @author ysun138
  *
  */
 public class VertexGeoReach {
-	public ArrayList<Integer> types;
-	public ArrayList<TreeSet<Integer>> ReachGrids;
-	public ArrayList<MyRectangle> RMBRs;
-	public ArrayList<Boolean> GeoBs;
-	
-	public VertexGeoReach(int MAX_HOP)
-	{
-		ReachGrids = new ArrayList<TreeSet<Integer>>(MAX_HOP);
-		RMBRs = new ArrayList<MyRectangle>(MAX_HOP);
-		GeoBs = new ArrayList<Boolean>(MAX_HOP);
-		for (int i = 0; i < MAX_HOP; i++)
-		{
-			ReachGrids.add(null);
-			RMBRs.add(null);
-			GeoBs.add(null);
-		}
-	}
-	
-	@Override
-	public String toString()
-	{
-		String string = "";
-		for ( int i = 0; i < ReachGrids.size(); i++)
-		{
-			string += ReachGrids.get(i);
-			string += ";" + RMBRs.get(i);
-			string += ";" + GeoBs.get(i);
-			string += "\n";
-		}
-		return string;
-	}
+  public ArrayList<Integer> types;
+  public ArrayList<TreeSet<Integer>> ReachGrids;
+  public ArrayList<MyRectangle> RMBRs;
+  public ArrayList<Boolean> GeoBs;
+
+  public VertexGeoReach(int MAX_HOP) {
+    ReachGrids = new ArrayList<TreeSet<Integer>>(MAX_HOP);
+    RMBRs = new ArrayList<MyRectangle>(MAX_HOP);
+    GeoBs = new ArrayList<Boolean>(MAX_HOP);
+    for (int i = 0; i < MAX_HOP; i++) {
+      ReachGrids.add(null);
+      RMBRs.add(null);
+      GeoBs.add(null);
+    }
+  }
+
+  @Override
+  public String toString() {
+    String string = "";
+    for (int i = 0; i < ReachGrids.size(); i++) {
+      string += ReachGrids.get(i);
+      string += ";" + RMBRs.get(i);
+      string += ";" + GeoBs.get(i);
+      string += "\n";
+    }
+    return string;
+  }
 }
