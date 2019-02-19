683387d64154481717a30ca4478565090faf4d9d
yuhansun <wdmzssyh@gmail.com>
2019-02-19 14:06:51 -0700

    usaage of filename

--- GeoReach_Traverse/src/main/java/experiment/AddEdge.java
+++ GeoReach_Traverse/src/main/java/experiment/AddEdge.java
@@ -1,30 +1,48 @@
 package experiment;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
 import java.util.TreeSet;
+import commons.Config;
 import commons.Entity;
 import commons.GraphUtil;
 import commons.ReadWriteUtil;
+import commons.Util;
 
 public class AddEdge {
 
-  public static void main(String[] args) {
+  public static Config config = new Config();
+
+  public static void main(String[] args) throws IOException {
     // TODO Auto-generated method stub
+    ClassLoader classLoader = new AddEdge().getClass().getClassLoader();
+    File file = new File(classLoader.getResource("").getFile());
+    String homeDir = file.getAbsolutePath();
+    String dataset = "Yelp";
+    String dataDir = homeDir + "/data/" + dataset;
+    String graphPath = dataDir + "/" + config.getGraphFileName();
+    String entityPath = dataDir + "/" + config.getEntityFileName();
+    String labelListPath = dataDir + "/" + config.getLabelListFileName();
+    String outputPath = "/Users/zhouyang/Google_Drive/Projects/GeoReachHop/add_edge/edges.txt";
+    generateEdges(graphPath, entityPath, labelListPath, 1000, outputPath);
 
   }
 
 
   public static void generateEdges(String graphPath, String entityPath, String labelListPath,
-      int targetCount, String outputPath) {
+      int targetCount, String outputPath) throws IOException {
+    Util.println("Read graph from " + graphPath);
     List<TreeSet<Integer>> graph =
         GraphUtil.convertListGraphToTreeSetGraph(GraphUtil.ReadGraph(graphPath));
+    Util.println("Read entity from " + entityPath);
     ArrayList<Entity> entities = GraphUtil.ReadEntity(entityPath);
+    Util.println("Read labels from " + labelListPath);
     ArrayList<Integer> labelList = ReadWriteUtil.readIntegerArray(labelListPath);
+    Util.println("Generate edges output to " + outputPath);
     generateEdges(graph, entities, labelList, targetCount, outputPath);
   }
 
@@ -38,7 +56,7 @@ public class AddEdge {
    * @param outputPath
    * @throws IOException
    */
-  public static void generateEdges(List<Set<Integer>> graph, List<Entity> entities,
+  public static void generateEdges(List<TreeSet<Integer>> graph, List<Entity> entities,
       List<Integer> labelList, int targetCount, String outputPath) throws IOException {
     FileWriter writer = new FileWriter(outputPath);
     Random random = new Random();
