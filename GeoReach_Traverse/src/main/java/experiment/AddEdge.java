ec7bcec0331fa02310e71346910a53cee5fd467c
yuhansun <wdmzssyh@gmail.com>
2019-02-19 23:20:51 -0700

    able to run one time test

--- GeoReach_Traverse/src/main/java/experiment/AddEdge.java
+++ GeoReach_Traverse/src/main/java/experiment/AddEdge.java
@@ -7,40 +7,160 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Random;
+import org.apache.commons.io.FileUtils;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
 import commons.Config;
+import commons.Edge;
 import commons.Entity;
 import commons.GraphUtil;
+import commons.Neo4jGraphUtility;
 import commons.ReadWriteUtil;
+import commons.SpaceManager;
 import commons.Util;
+import construction.Maintenance;
+import dataprocess.LoadData;
 
 public class AddEdge {
 
   public static Config config = new Config();
 
-  public static String homeDir = null, dataDir = null;
-  public static String dataset = null;
-  public static String graphPath, entityPath, labelListPath;
+  /**
+   * Paths related to data files, including db.
+   */
+  public String homeDir, dataDir;
+  public String dataset;
+  public String graphPath, entityPath, labelListPath;
+  public String dbPath;
+
+  /**
+   * Paths related to experiment.
+   */
+  public String experimentDir;// root dir of queryDir and resultDir
+  public String queryDir; // contains the edges
+  public String resultDir;// contains the result
+
+  ArrayList<ArrayList<Integer>> graph = null;
+  ArrayList<Entity> entities = null;
+  ArrayList<Integer> labelList = null;
+
+  String mapPath;
+  long[] graph_pos_map_list;
 
   public static void main(String[] args) throws Exception {
     // TODO Auto-generated method stub
-    iniPaths();
-    generateEdges();
+    AddEdge addEdge = new AddEdge();
+    addEdge.iniPaths();
+
+    // generateEdges();
+    addEdge.evaluate();
   }
 
-  public static void iniPaths() {
+  public void iniPaths() {
+    // Only need to modify homeDir.
     ClassLoader classLoader = new AddEdge().getClass().getClassLoader();
     File file = new File(classLoader.getResource("").getFile());
     homeDir = file.getAbsolutePath();
+
+    // data dirs
     dataset = "Yelp";
     dataDir = homeDir + "/data/" + dataset;
     graphPath = dataDir + "/" + config.getGraphFileName();
     entityPath = dataDir + "/" + config.getEntityFileName();
     labelListPath = dataDir + "/" + config.getLabelListFileName();
+
+    // experiment dirs
+    experimentDir = "/Users/zhouyang/Google_Drive/Projects";
+    queryDir = String.format("%s/query/%s", experimentDir, dataset);
+    resultDir = experimentDir + "/add_edge";
   }
 
+  public static int piecesX = 128, piecesY = 128;
+  public static int MC = 0;
+  public static int MAX_HOP = 1;
+  public static double minx = -180, miny = -90, maxx = 180, maxy = 90;
 
-  public static void generateEdges() throws Exception {
-    String outputPath = "/Users/zhouyang/Google_Drive/Projects/GeoReachHop/add_edge/edges.txt";
+  public void evaluate() throws Exception {
+    double MG = 2.0, MR = 2.0;
+    evaluate(MG, MR, MC);
+
+  }
+
+  public void evaluate(double MG, double MR, int MC) throws Exception {
+    String dbFileName = Neo4jGraphUtility.getDbNormalName(piecesX, piecesY, MG, MR, MC);
+    if (!Util.pathExist(dataDir)) {
+      new File(dataDir).mkdirs();
+    }
+
+    dbPath = dataDir + "/" + dbFileName;
+    // mapPath is initialized before loadGraphAndIndex because its value will be used.
+    mapPath = dataDir + "/"
+        + Neo4jGraphUtility.getGraphNeo4jIdMapNormalName(piecesX, piecesY, MG, MR, MC);
+    if (!Util.pathExist(dbPath)) {
+      loadGraphAndIndex();
+    }
+    graph_pos_map_list = ReadWriteUtil.readMapToArray(mapPath);
+
+    String testDbFileName = "test_" + dbFileName;
+    String testDbPath = dataDir + "/" + testDbFileName;
+    // If the testDb dir exists, remove it.
+    // Because it was inserted edges and is different from the original graph.
+    if (Util.pathExist(testDbPath)) {
+      FileUtils.deleteDirectory(new File(dbPath));
+    }
+    // Copy the original db to the test db dir.
+    FileUtils.copyDirectoryToDirectory(new File(dbPath), new File(testDbPath));
+
+    GraphDatabaseService service = Neo4jGraphUtility.getDatabaseService(testDbPath);
+    SpaceManager spaceManager = new SpaceManager(minx, miny, maxx, maxy, piecesX, piecesY);
+    Maintenance maintenance = new Maintenance(spaceManager, MAX_HOP, MG, MR, MC, service);
+    List<Edge> edges = GraphUtil.readEdges(queryDir + "/edges.txt");
+    addEdgeMaintenance(edges, maintenance);
+    service.shutdown();
+  }
+
+  private void loadGraphAndIndex() throws Exception {
+    readGraph();
+    LoadData loadData = new LoadData();
+    loadData.loadAllEntityAndCreateIdMap(entities, labelList, dbPath, mapPath);
+    loadData.LoadGraphEdges(mapPath, dbPath, graph);
+  }
+
+  private void readGraph() {
+    graph = GraphUtil.ReadGraph(graphPath);
+    entities = GraphUtil.ReadEntity(entityPath);
+    labelList = ReadWriteUtil.readIntegerArray(labelListPath);
+  }
+
+  /**
+   * <code>edges</code> contains the long type neo4j id.
+   *
+   * @param edges
+   * @param maintenance
+   * @throws Exception
+   */
+  public static void addEdgeMaintenance(List<Edge> edges, Maintenance maintenance)
+      throws Exception {
+    GraphDatabaseService service = maintenance.service;
+    Transaction tx = service.beginTx();
+    for (Edge edge : edges) {
+      Node src = service.getNodeById(edge.start);
+      Node trg = service.getNodeById(edge.end);
+      maintenance.addEdgeAndUpdateIndex(src, trg);
+    }
+    tx.success();
+    tx.close();
+  }
+
+  /**
+   * Generate the edges for experiment.
+   *
+   * @throws Exception
+   */
+  public void generateEdges() throws Exception {
+    String outputPath =
+        "/Users/zhouyang/Google_Drive/Projects/GeoReachHop/query/" + dataset + "/edges.txt";
     generateEdges(graphPath, entityPath, labelListPath, 0.05, outputPath);
   }
 
