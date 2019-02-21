991fd815842cb02bcf43a201e580e01e94b83874
yuhansun <wdmzssyh@gmail.com>
2019-02-21 14:24:55 -0700

    RB insert test

--- GeoReach_Traverse/src/test/java/commons/ArrayUtilTest.java
+++ GeoReach_Traverse/src/test/java/commons/ArrayUtilTest.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.roaringbitmap.RoaringBitmap;
 
 public class ArrayUtilTest {
   @BeforeClass
@@ -27,5 +29,22 @@ public class ArrayUtilTest {
     assertTrue(ArrayUtil.isSortedListEqual(l1, l2));
     l2.add(3);
     assertFalse(ArrayUtil.isSortedListEqual(l1, l2));
+  }
+
+  @Test
+  public void roaringBitmapTest() {
+    RoaringBitmap rBitmap = new RoaringBitmap();
+    rBitmap.add(-100);
+    rBitmap.add(-10);
+    Util.println(rBitmap);
+    Iterator<Integer> iterator = rBitmap.iterator();
+    while (iterator.hasNext()) {
+      Util.println(iterator.next());
+    }
+    // rBitmap.add(50);
+    // Util.println(rBitmap);
+    // rBitmap.add();
+    // Util.println(rBitmap);
+
   }
 }
