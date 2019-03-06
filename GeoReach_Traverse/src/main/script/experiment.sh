bf7880fb7aa59f5344ca544314b2600bc5a568f2
yuhansun <wdmzssyh@gmail.com>
2019-03-05 22:55:05 -0700

    update

--- GeoReach_Traverse/src/main/script/experiment.sh
+++ GeoReach_Traverse/src/main/script/experiment.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # server
-dir="/hdd/code/yuhansun"
+homeDir="/hdd/code/yuhansun"
 dataset="Yelp"
-data_dir="${dir}/data/${dataset}"
-code_dir="${dir}/code"
+# data_dir="${dir}/data/${dataset}"
+code_dir="${homeDir}/code"
+resultDir="${homeDir}/result"
 
 # local test setup
 # dir="/Users/zhouyang/Google_Drive/Projects/tmp/risotree"
@@ -16,5 +17,14 @@ jar_path="${code_dir}/Riso-Tree/target/Riso-Tree-0.0.1-SNAPSHOT.jar"
 echo "java -Xmx100g -jar ${jar_path} -h"
 java -Xmx100g -jar ${jar_path} -h
 
-echo "java -Xmx100g -jar ${jar_path} -f tree -dp ${db_path} -d ${dataset} -gp ${graph_path} -ep ${entity_path} -lp ${label_path}"
-java -Xmx100g -jar ${jar_path} -f tree -dp ${db_path} -d ${dataset} -gp ${graph_path} -ep ${entity_path} -lp ${label_path}
+# run the query
+echo "java -Xmx100g -jar ${jar_path} -f inser -dp ${db_path} -d ${dataset} -gp ${graph_path} -ep ${entity_path} -lp ${label_path}"
+# create the db first
+java -Xmx100g -jar ${jar_path} -f insertion -hd ${homeDir} -rd ${resultDir} -d ${dataset} -MG 1.0 -MR 2.0 -testRatio 0.5 -us LIGHTWEIGHT
+# run the query
+java -Xmx100g -jar ${jar_path} -f query -hd ${homeDir} -rd ${resultDir} -d ${dataset} -MG 1.0 -MR 2.0 -us LIGHTWEIGHT -ex SPATRAVERSAL
+
+
+# java -Xmx100g -jar ${jar_path} -f insertion -hd ${homeDir} -rd ${resultDir} -d ${dataset} -MG 1.0 -MR 2.0 -testRatio 0.5 -e LIGHTWEIGHT
+# java -Xmx100g -jar ${jar_path} -f insertion -hd ${homeDir} -rd ${resultDir} -d ${dataset} -MG 1.0 -MR 2.0 -testRatio 0.5 -e LIGHTWEIGHT
+# java -Xmx100g -jar ${jar_path} -f insertion -hd ${homeDir} -rd ${resultDir} -d ${dataset} -MG 1.0 -MR 2.0 -testRatio 0.5 -e LIGHTWEIGHT
