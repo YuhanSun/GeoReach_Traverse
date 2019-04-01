4c4d40eb60fb798a12929f5449a49f0d3f0ec825
yuhansun <wdmzssyh@gmail.com>
2019-04-01 01:56:50 -0700

    ignore

--- GeoReach_Traverse/src/main/script/wikidata_process.sh
+++ GeoReach_Traverse/src/main/script/wikidata_process.sh
@@ -20,15 +20,15 @@ java -Xmx100g -jar ${jar_path} -h
 
 # java -Xmx100g -jar ${jar_path} -f wikicutLabelFile -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
 # java -Xmx100g -jar ${jar_path} -f wikicutPropertyAndEdge -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
-java -Xmx100g -jar ${jar_path} -f wikicutDescription -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+# java -Xmx100g -jar ${jar_path} -f wikicutDescription -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
 
 # java -Xmx100g -jar ${jar_path} -f wikidataExtractProperties -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
 # java -Xmx100g -jar ${jar_path} -f wikidataExtractStringLabel -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
-# java -Xmx100g -jar ${jar_path} -f extractEntityToEntityRelationEdgeFormat -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+java -Xmx100g -jar ${jar_path} -f extractEntityToEntityRelationEdgeFormat -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
 
 # java -Xmx100g -jar ${jar_path} -f wikidataLoadGraph -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
-# java -Xmx100g -jar ${jar_path} -f wikiLoadEdges -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
-# java -Xmx100g -jar ${jar_path} -f wikiLoadAttributes -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+java -Xmx100g -jar ${jar_path} -f wikiLoadEdges -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+java -Xmx100g -jar ${jar_path} -f wikiLoadAttributes -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
 
 # java -Xmx100g -jar ${jar_path} -f wikirecoverSpatialProperty -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
 # java -Xmx100g -jar ${jar_path} -f wikirecoverName -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
