684a988998f7797bb9a56ba035e4141af8a87966
yuhansun <wdmzssyh@gmail.com>
2019-04-22 12:47:42 -0700

    ini the bash file for risotree data generation

---
+++ GeoReach_Traverse/src/main/script/wikidata_process_risotree.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+./package.sh
+# server
+homeDir="/hdd/code/yuhansun"
+
+dataset="wikidata"
+
+data_dir="${homeDir}/data/${dataset}"
+code_dir="${homeDir}/code"
+resultDir="${homeDir}/result"
+dbPath="${data_dir}/neo4j-community-3.1.1/data/databases/graph.db"
+
+# local test setup
+# dir="/Users/zhouyang/Google_Drive/Projects/tmp/risotree"
+# dataset="Yelp"
+# data_dir="${dir}/${dataset}"
+# code_dir="/Users/zhouyang/Google_Drive/Projects/github_code"
+
+jar_path="${code_dir}/GeoReach_Traverse/GeoReach_Traverse/target/GeoReach_Traverse-0.0.1-SNAPSHOT.jar"
+echo "java -Xmx100g -jar ${jar_path} -h"
+java -Xmx100g -jar ${jar_path} -h
+
+# java -Xmx100g -jar ${jar_path} -f wikicutLabelFile -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+# java -Xmx100g -jar ${jar_path} -f wikicutPropertyAndEdge -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+# java -Xmx100g -jar ${jar_path} -f wikicutDescription -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+
+# java -Xmx100g -jar ${jar_path} -f wikidataExtractProperties -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+# java -Xmx100g -jar ${jar_path} -f wikidataExtractStringLabel -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+# java -Xmx100g -jar ${jar_path} -f extractEntityToEntityRelation -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+# java -Xmx100g -jar ${jar_path} -f extractEntityToEntityRelationEdgeFormat -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+
+java -Xmx100g -jar ${jar_path} -f wikidataLoadGraph -hd ${data_dir} -dp ${dbPath}
+java -Xmx100g -jar ${jar_path} -f wikiLoadEdges -hd ${data_dir} -dp ${dbPath}
+# java -Xmx100g -jar ${jar_path} -f wikiLoadAttributes -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+
+# Not used because entity_properties.txt contains all these information.
+# java -Xmx100g -jar ${jar_path} -f wikirecoverSpatialProperty -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
+# java -Xmx100g -jar ${jar_path} -f wikirecoverName -hd ${data_dir} -rd ${resultDir} -d ${dataset} -MG 2.0 -MR 2.0
